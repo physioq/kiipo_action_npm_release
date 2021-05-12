ff96a8c9fda4af79960369757ee41b22d63e23c9
Duncan Marsh <slugbyte@slugbyte.com>
2021-05-12 19:34:55 +0800

    [feature] added 5000 ms sleep before enable

--- src/index.js
+++ src/index.js
@@ -4,9 +4,12 @@ import {format} from 'date-fns'
 import {version} from '../package.json'
 import {basename} from 'path'
 
+const sleep = (ms) =>  new Promise((resolve) => setTimeout(ms, resolve))
+
 const GITHUB_TOKEN = core.getInput('github_token')
 const WORKFLOW_FILENAME = core.getInput('workflow_filename')
 const ENABLE_OR_DISABLE = core.getInput('enable_or_disable')
+const ENABLE_MILISECOND_DELAY = 5000
 
 const ENDPOINT = {
   DISABLE: 'PUT /repos/{owner}/{repo}/actions/workflows/{workflow_filename}/disable',
@@ -18,6 +21,8 @@ export const main = async () => {
   const {payload} = github.context
 
   const endpoint = ENABLE_OR_DISABLE == 'enable' ? ENDPOINT.ENABLE : ENDPOINT.DISABLE
+
+  if (endpoint == 'enable') await sleep(ENABLE_MILISECOND_DELAY)
 
   await octokit.request(endpoint, {
     owner: payload.organization.login,
